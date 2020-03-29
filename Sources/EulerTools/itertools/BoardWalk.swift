0efd712a75fe0424da1d98865337c4d1a93b7418
John Griffin <John_griffin@hotmail.com>
2020-03-28 20:06:29 -0700

    BoardWalk

---
+++ Sources/EulerTools/itertools/BoardWalk.swift
@@ -0,0 +1,166 @@
+//
+//  BoggleWalk.swift
+//
+//
+//  Created by John Griffin on 3/28/20.
+//
+
+import Foundation
+
+/**
+ In the game of bogle, you've got an MxN board where you can move in any direction, but can't revisit a square
+ The generalized verision of this is common enought to deserve class
+ */
+public class BoardWalk {
+    public let boardSize: BoardSize
+    public let allowedSteps: [Step]
+
+    public init(boardSize: BoardSize, allowedSteps: [Step]) {
+        self.boardSize = boardSize
+        self.allowedSteps = allowedSteps
+    }
+
+    public typealias BoardSize = (rows: Int, cols: Int)
+    public typealias Step = (dx: Int, dy: Int)
+
+    public static let squareSteps: [Step] = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+    public static let diagonalSteps: [Step] = [(1, -1), (1, 1), (-1, 1), (-1, 1)]
+    public static let squareAndDiagonalSteps: [Step] = squareSteps + diagonalSteps
+}
+
+extension BoardWalk {
+    public typealias Path = [Square]
+
+    public struct Square: Equatable, CustomStringConvertible {
+        public let x, y: Int
+
+        public init(_ x: Int, _ y: Int) {
+            self.x = x
+            self.y = y
+        }
+
+        public func move(_ step: Step) -> Square {
+            return Square(x + step.dx, y + step.dy)
+        }
+
+        public func ifValidOn(_ boardSize: BoardSize) -> Square? {
+            guard x.isBetween(lower: 0, upper: boardSize.cols),
+                y.isBetween(lower: 0, upper: boardSize.rows) else {
+                return nil
+            }
+            return self
+        }
+
+        public var description: String { "(\(x),\(y))" }
+    }
+}
+
+extension BoardWalk {
+    public class Stepper: IteratorProtocol, Sequence {
+        public let square: Square
+        public let board: BoardWalk
+
+        public init(from square: Square,
+                    onBoard board: BoardWalk) {
+            self.square = square
+            self.board = board
+        }
+
+        // MARK: iteratorprotocol
+
+        var stepIterator: IndexingIterator<[Step]>!
+
+        public func next() -> Square? {
+            while true {
+                if stepIterator == nil {
+                    stepIterator = board.allowedSteps.makeIterator()
+                }
+
+                guard let nextStep = stepIterator.next() else {
+                    return nil
+                }
+                guard let nextSquare = square
+                    .move(nextStep)
+                    .ifValidOn(board.boardSize) else {
+                    continue
+                }
+                return nextSquare
+            }
+        }
+
+        // MARK: sequence
+
+        public func makeIterator() -> BoardWalk.Stepper {
+            return Stepper(from: square, onBoard: board)
+        }
+    }
+
+    public class Walker: IteratorProtocol, Sequence {
+        let staringSquare: Square
+        let board: BoardWalk
+        public let isDeadEndPath: IsDeadEndPath?
+
+        // for many algorithms, we know the path isn't going to be interesting
+        // return true to prune the branch
+        public typealias IsDeadEndPath = (Path) -> Bool
+
+        public init(from square: Square,
+                    onBoard board: BoardWalk,
+                    isDeadEndPath: IsDeadEndPath?) {
+            staringSquare = square
+            self.board = board
+            self.isDeadEndPath = isDeadEndPath
+        }
+
+        // MARK: iteratorprotocol
+
+        var squareSteppers: [Stepper]!
+
+        public func next() -> Path? {
+            while true {
+                if squareSteppers == nil {
+                    // start from the beginning
+                    squareSteppers = [Stepper(from: staringSquare, onBoard: board)]
+                }
+
+                guard !squareSteppers.isEmpty else {
+                    // we've gone through and backtracked and we're done
+                    return nil
+                }
+
+                guard let nextSquare = squareSteppers.last!.next() else {
+                    // we've exhaused this stepper, backtrack
+                    squareSteppers.removeLast()
+                    continue
+                }
+
+                guard !squareSteppers.contains(where: { $0.square == nextSquare }) else {
+                    // we've been there before, keep looking
+                    continue
+                }
+
+                // setup stepper for next iteration
+                squareSteppers.append(Stepper(from: nextSquare, onBoard: board))
+
+                let path = squareSteppers.map { $0.square }
+
+                if isDeadEndPath?(path) == true {
+                    // prune this path
+                    squareSteppers.removeLast()
+                    continue
+                }
+
+                // but first return this square
+                return path
+            }
+        }
+
+        // MARK: sequence
+
+        public func makeIterator() -> Walker {
+            return Walker(from: staringSquare,
+                          onBoard: board,
+                          isDeadEndPath: isDeadEndPath)
+        }
+    }
+}
---
+++ Tests/EulerToolsTests/itertools/BoardWalkTests.swift
@@ -0,0 +1,70 @@
+//
+//  BoggleWalkTests.swift
+//
+//
+//  Created by John Griffin on 3/28/20.
+//
+
+import EulerTools
+import XCTest
+
+class BoardWalkTests: XCTestCase {
+    typealias Sq = BoardWalk.Square
+    typealias Path = [Sq]
+    
+    
+    let boardWalk3x3Square = BoardWalk(boardSize: (rows: 3, cols: 3),
+                                        allowedSteps: BoardWalk.squareSteps)
+    
+    func testStepper() {
+        let tests: [(start: Sq, check: [Sq])] = [
+            (Sq(0, 0), [Sq(0, 1), Sq(1, 0)]),
+            (Sq(1, 0), [Sq(1, 1), Sq(0, 0), Sq(2, 0)]),
+            (Sq(2, 0), [Sq(2, 1), Sq(1, 0)]),
+            (Sq(0, 1), [Sq(0, 0), Sq(0, 2), Sq(1, 1)]),
+            (Sq(1, 1), [Sq(1, 0), Sq(1, 2), Sq(0, 1), Sq(2, 1)]),
+            (Sq(2, 1), [Sq(2, 0), Sq(2, 2), Sq(1, 1)]),
+            (Sq(0, 2), [Sq(0, 1), Sq(1, 2)]),
+            (Sq(1, 2), [Sq(1, 1), Sq(0, 2), Sq(2, 2)]),
+            (Sq(2, 2), [Sq(2, 1), Sq(1, 2)]),
+        ]
+        tests.forEach { test in
+            let stepper = BoardWalk.Stepper(from: test.start, onBoard: boardWalk3x3Square)
+            XCTAssertEqual(Array(stepper), test.check)
+        }
+    }
+    
+    func testWalker() {
+        let walker = BoardWalk.Walker(from: Sq(0, 0),
+                                      onBoard: boardWalk3x3Square,
+                                      isDeadEndPath: { path in path.count > 3 })
+        let paths = Array(walker)
+        let boogglePaths = paths.map { bogglePath($0) }
+
+        let check: [Path] = [
+            [Sq(0, 0), Sq(0, 1)],
+            [Sq(0, 0), Sq(0, 1), Sq(0, 2)],
+            [Sq(0, 0), Sq(0, 1), Sq(1, 1)],
+            [Sq(0, 0), Sq(1, 0)],
+            [Sq(0, 0), Sq(1, 0), Sq(1, 1)],
+            [Sq(0, 0), Sq(1, 0), Sq(2, 0)],
+        ]
+        
+        
+        XCTAssertEqual(paths, check)
+        XCTAssertEqual(boogglePaths, ["ad", "adg", "ade", "ab", "abe", "abc"])
+    }
+    
+    
+    func bogglePath(_ path: Path) -> String {
+        String(path.map { boggleLetter($0) })
+    }
+
+    static let boogleLetters = Array("abcdefghi")
+    
+    func boggleLetter(_ square: Sq) -> Character {
+        let boogleIndex = square.y * 3 + square.x
+        return Self.boogleLetters[boogleIndex]
+    }
+
+}
